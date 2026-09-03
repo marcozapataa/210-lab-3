013415278818f976258d5de3a3a2734af1f8ec34
Marco <mazapata053@gmail.com>
2026-09-03 14:19:06 -0700

    finished function prototype definition

--- main.cpp
+++ main.cpp
@@ -12,7 +12,37 @@ struct Restaurant
     int seatingCapacity;
 };
 
+//function prototype
+Restaurant inputRestaurant();
+
 int main()
 {
+    Restaurant userRest = inputRestaurant();
+
     
+
+    return 0;
+}
+
+// inputRestaurant definition
+Restaurant inputRestaurant()
+{
+    Restaurant temp;
+
+    cout << "Enter restaurant name: ";
+    getline(cin, temp.name);
+
+    cout << "Enter restaurant address: ";
+    getline(cin, temp.address);
+
+    cout << "Enter restaurant phone number: ";
+    getline(cin, temp.phoneNr);
+
+    cout << "Enter restaurant rating out of 5: ";
+    cin >> temp.rating;
+
+    cout << "Enter seating capacity: ";
+    cin >> temp.seatingCapacity;
+
+    return temp;
 }
