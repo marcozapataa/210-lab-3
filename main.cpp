37cebe6c3a78da8269ce593dd6c85c72f2563897
Marco <mazapata053@gmail.com>
2026-09-03 13:56:26 -0700

    Added Restaurant struct definition

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Restaurant
+{
+    string name; 
+    string address;
+    string phoneNr;  //phone number
+    double rating;
+    int seatingCapacity;
+};
+
+int main()
+{
+    
+}
